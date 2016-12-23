6af80657ad7a2d0448f97a4ed8a4ec193690c55d
Jayden Chen <chenzhenhaojayden@gmail.com>
2016-12-23 16:02:11 +0800

    fix bug in hash and prefix

--- WechatProcessor/functions.py
+++ WechatProcessor/functions.py
@@ -4,10 +4,10 @@ import hashlib
 
 def hash_md5_string(string):
     md5 = hashlib.md5()
-    md5.update(string)
+    md5.update(string.encode("utf-8"))
     return md5.hexdigest()
 
 def hash_sha1_string(string):
     md5 = hashlib.sha1()
-    md5.update(string)
+    md5.update(string.encode("utf-8"))
     return md5.hexdigest()
